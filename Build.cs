51d5375cc70d87d626b9da11e93c3b33f959a07d
sashasuro <123554714+sashasuro@users.noreply.github.com>
2024-02-18 23:00:08 +0300

    Add files via upload

---
+++ Build.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Build
+    {
+        public string name;
+        public double area;
+        public int kvo;
+        public Build()
+        {
+
+        }
+        public Build(string name, double area, int kvo)
+        {
+            this.name = name;
+            this.area = area;
+            this.kvo = kvo;
+        }
+        static void ShowInfo(string[] args)
+        {
+            Build build1 = new Build();
+            Build build2 = new Build("Частный дом", 145, 4);
+        }
+    }
+}
