44a50d6eef1a20a8fc8ceb72a7add4ce3c13c6d8
sashasuro <123554714+sashasuro@users.noreply.github.com>
2024-02-18 22:31:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //static void Main(string[] args)
+            //{
+            // SayHello();
+            // SayGoodbuy();
+            // Console.Read();
+            //}
+            //static void SayHello()
+            //{
+            // Console.WriteLine("Hello");
+            //}
+            //static void SayGoodbuy()
+            //{
+            // Console.WriteLine("Goodbuy");
+            //}
+
+            //ex.1
+            //static void Func()
+            //{
+            // Console.Write("x=");
+            // int x = int.Parse(Console.ReadLine());
+            // int y = x * x;
+            // Console.WriteLine($"y={y}");
+            //}
+            //static void Main()
+            //{
+            // Func();
+            // Console.Read();
+            //}
+
+            //ex.2
+            //public static int Add(int x, int y)
+            //{
+            // int sum = x + y;
+            // Console.WriteLine($"x={x}\ty={y}");
+            // x = 100;
+            // y = 500;
+            // Console.WriteLine($"{x}\t{y}");
+            // return sum;
+            //}
+            //static void Main(string[] args)
+            //{
+            // int x = 9, y = 10;
+            // Console.WriteLine($"x={x}\ty={y}");
+            // int z = Add(x, y);
+            // Console.WriteLine($"z= {z}");
+            // Console.WriteLine($"x= {x}\ty={y}");
+            // Console.Read();
+            //}
+
+            //ex.6
+            //public static int Add(int x, int y)
+            //{
+            // return x + y;
+            //}
+            //public static int Dif(int x, int y)
+            //{
+            // return x - y;
+            //}
+            //public static int Mult(int x, int y)
+            //{
+            // return x * y;
+            //}
+            //public static double Div(int x, int y)
+            //{
+            // return x / y;
+            //}
+            //static void Main(string[] args)
+            //{
+            // Console.Write("x = ");
+            // int x = int.Parse(Console.ReadLine());
+            // Console.Write("y = ");
+            // int y = int.Parse(Console.ReadLine());
+            // int sum = Add(x, y);
+            // Console.WriteLine($"Сумма = {sum}");
+            // int razn = Dif(x, y);
+            // Console.WriteLine($"Разность = {razn}");
+            // int proiz = Mult(x, y);
+            // Console.WriteLine($"Произведене = {proiz}");
+            // double del = Div(x, y);
+            // Console.WriteLine($"Деление = {del}");
+            //}
+
+        }
+    }
