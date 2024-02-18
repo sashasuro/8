4a47ca15db616abdc324e48f3cb641d87a161167
sashasuro <123554714+sashasuro@users.noreply.github.com>
2024-02-18 23:04:56 +0300

    Add files via upload

---
+++ Programm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student("Ванёк", 3);
+            st1.ShowInfo();
+            Student st2 = new Student("", -7);
+            st2.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Student
+    {
+        private string fam;
+        private int kurs;
+
+        public string Fam
+        {
+            get
+            {
+                return (fam != "") ? fam : "Неизвестный";
+            }
+            set
+            {
+                fam = value.ToUpper();
+            }
+        }
+
+        public int Kurs
+        {
+            get
+            {
+                return kurs < 1 || kurs > 4 ? 0 : kurs;
+            }
+            set
+            {
+                kurs = value;
+            }
+        }
+
+        public Student(string fam, int kurs)
+        {
+            this.Fam = fam;
+            this.Kurs = kurs;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Фамилия:\t{Fam}\nКурс\t{Kurs}");
+        }
+    }
+}
