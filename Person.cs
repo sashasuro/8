77d3f676d73eb13eaed84f8eaa0cadbcff798af5
sashasuro <123554714+sashasuro@users.noreply.github.com>
2024-02-18 22:57:50 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Person
+    {
+        public string name;
+            public string surname;
+            public int age;
+            public Person()
+            { }
+
+
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person("Андрей", "Мустяца", 17);
+        }
+
+
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Point
+    {
+        int x, y;
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return "x=" + x + " y=" + y;
+        }
+
+        public double Distance()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
